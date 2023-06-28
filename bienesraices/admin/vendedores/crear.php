bcac74739286ce41534dd9f005bc9e98e305d89f
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-28 04:28:22 -0600

    Final

--- bienesraices/admin/vendedores/crear.php
+++ bienesraices/admin/vendedores/crear.php
@@ -3,7 +3,7 @@
     echo $_POST['nombre'];
     echo $_POST['apellido'];
     echo $_POST['telefono'];
-    include ('../bienesraices/aux2.php');
+    include ('./aux2.php');
     if(isset($_POST['nombre'],$_POST['apellido'],$_POST['telefono'])){
         echo "entra";
         $dao = new DAO();
