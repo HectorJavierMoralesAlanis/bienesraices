825c8a3145a6933ceab6dc0644f38bd7319f3879
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-28 05:39:11 -0600

    Final

--- bienesraices/admin/propiedades/crear.php
+++ bienesraices/admin/propiedades/crear.php
@@ -8,7 +8,7 @@
     if(isset($_POST['titulo'],$_POST['precio'],$_POST['imagen'],$_POST['descripcion'],$_POST['habitaciones'],$_POST['wc'],$_POST['estacionamiento'],$_POST['vendedor'])){
         $dao = new DAO();
         $fecha=date('Y-m-d H:i:s');
-        $consulta="INSERT INTO propiedades (titulo,precio,imagen,descripcion,habitaciones,wc,estacionamiento,creado,vendedores_id)"."VALUES(:titulo,:precio,:imagen,:descripcion,:habitaciones,:wc,:estacionamiento,:fecha,:id_vendedores)";
+        $consulta="INSERT INTO Propiedades (titulo,precio,imagen,descripcion,habitaciones,wc,estacionamiento,creado,vendedores_id)"."VALUES(:titulo,:precio,:imagen,:descripcion,:habitaciones,:wc,:estacionamiento,:fecha,:id_vendedores)";
         $parametros=array("titulo"=>"$_POST[titulo]",
                         "precio"=>"$_POST[precio]",
                         "imagen"=>"$_POST[imagen]",
