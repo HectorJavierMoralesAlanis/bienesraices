763e5b6a27ebd51f441ea697ce8280c6e84c4630
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-28 02:27:50 -0600

    Final

--- bienesraices/incio.php
+++ bienesraices/incio.php
@@ -9,8 +9,7 @@
         <div class="iconos-nosotros">
             <div class="icono">
                 <img src="build/img/icono1.svg" alt="Icono seguridad" loading="lazy">
-                <h3>Seguridad</h3>
-                <p>Possimus, suscipit repudiandae. Autem deserunt aliquid deleniti sit minus consectetur obcaecati molestiae dolorem natus dolores reiciendis tempore, explicabo cum nobis laudantium. Voluptates?</p>
+                <h3><a href="/entrada.php">Seguridad</a></h3>
             </div>
             <div class="icono">
                 <img src="build/img/icono2.svg" alt="Icono Precio" loading="lazy">
