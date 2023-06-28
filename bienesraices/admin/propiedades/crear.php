45a38b2819376b55ecc37960747f430c73db2112
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-28 05:27:38 -0600

    Final

--- bienesraices/admin/propiedades/crear.php
+++ bienesraices/admin/propiedades/crear.php
@@ -4,6 +4,7 @@
     $dao2=new DAO();
     $consulta2="SELECT * FROM vendedores";
     $user_access=$dao2->ejecutarConsulta($consulta2);
+    echo $_POST['vendedor'];
     if(isset($_POST['titulo'],$_POST['precio'],$_POST['imagen'],$_POST['descripcion'],$_POST['habitaciones'],$_POST['wc'],$_POST['estacionamiento'],$_POST['vendedor'])){
         $dao = new DAO();
         $fecha=date('Y-m-d H:i:s');
