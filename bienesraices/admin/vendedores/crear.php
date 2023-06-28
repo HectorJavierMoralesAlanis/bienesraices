4e2f0f846163e17d72485af34389188176b96262
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-28 04:14:07 -0600

    Final

--- bienesraices/admin/vendedores/crear.php
+++ bienesraices/admin/vendedores/crear.php
@@ -3,7 +3,7 @@
     echo $_POST['nombre'];
     echo $_POST['apellido'];
     echo $_POST['telefono'];
-    include_once('/PDO/DAO.php');
+    include_once('../bienesraices/PDO/DAO.php');
     if(isset($_POST['nombre'],$_POST['apellido'],$_POST['telefono'])){
         echo "entra";
         $dao = new DAO();
