78a2e5e7db1b26c97073eb8653a6779e2eaf8b26
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-28 02:47:15 -0600

    Final

--- bienesraices/admin/index.php
+++ bienesraices/admin/index.php
@@ -5,7 +5,6 @@
 
     <main class="contenedor seccion">
         <h1>Administrador de Bienes Raices</h1>
-
         <a href="../../bienesraices/admin/propiedades/crear.php"  class="boton boton-verde">Nueva propiedad</a>
     </main>
 
