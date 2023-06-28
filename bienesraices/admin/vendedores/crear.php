c6d0188ae6912a37cd1ed58134a9d34f23bee5dd
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-28 04:37:54 -0600

    Final

--- bienesraices/admin/vendedores/crear.php
+++ bienesraices/admin/vendedores/crear.php
@@ -17,7 +17,7 @@
         if($resultados>=0){
             header("http://143.198.163.107/bienesraices/admin/index.php");
         }else{
-            header("http://143.198.163.107/bienesraices/admin/index.php");
+            echo "error";
         }
     }else{
         echo "error";
