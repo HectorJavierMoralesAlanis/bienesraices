8a5b69d0a22dd873a88858e20f4e5e75ac13fdef
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-28 19:55:20 -0600

    Final

--- bienesraices/admin/propiedades/crear.php
+++ bienesraices/admin/propiedades/crear.php
@@ -36,3 +36,56 @@
     require '../../includes/funciones.php';
     incluirTemplates('header');
 ?>
+    <main class="contenedor seccion">
+        <h1>Crear</h1>
+
+        <a href="/bienesraices/admin/index.php" class="boton boton-verde">Volver</a>
+
+        <form class="formulario" method="POST" action="/../bienesraices/admin/propiedades/crear.php" enctype="multipart/fomr-data">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad">
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" name="precio" placeholder="Precio Propiedad">
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" name="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">Descripción:</label>
+                <textarea id="descripcion" name="descripcion"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Información Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="text" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="wc">Baños:</label>
+                <input type="text" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="text" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+                
+                <select name="vendedor">
+                    <option selected>Opciones</option>
+                    <?php foreach($user_access as $row){?>
+                        <option value="<?php echo $row['id']?>"><?php echo $row['nombre']?></option>
+                    <?php }?>
+                </select>
+            </fieldset>
+
+            <button type="sumbit" class="boton boton-verde">Crear Propiedad</button>
+        </form>
+    </main>
+
+<?php
+    incluirTemplates('footer');
+?>
