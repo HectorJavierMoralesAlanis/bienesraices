2107e0ccbef8ff9d7265b341c38cdae9cedd4bb2
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-29 03:18:49 -0600

    Final

--- bienesraices/admin/propiedades/crear.php
+++ bienesraices/admin/propiedades/crear.php
@@ -9,7 +9,7 @@
 
         $secureId = strtoupper(bin2Hex(random_bytes(32)));
         $rutaArchivoTemp = $_FILES['imagen']['tmp_name'];
-        $rutaArchivoAGuardar = DIR_UPLOADS.$secureId;
+        $rutaArchivoAGuardar = DIR_UPLOADS.$secureId.'.jpg';
         move_uploaded_file($rutaArchivoTemp,$rutaArchivoAGuardar);
         $dao = new DAO();
         
@@ -19,7 +19,7 @@
         $parametros = array(
             "titulo" => $_POST['titulo'],
             "precio" => $_POST['precio'],
-            "imagen" => $secureId.$extencion,
+            "imagen" => $secureId,
             "descripcion" => $_POST['descripcion'],
             "habitaciones" => $_POST['habitaciones'],
             "wc" => $_POST['wc'],
