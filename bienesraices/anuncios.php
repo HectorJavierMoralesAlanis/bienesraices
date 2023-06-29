aaeee8bc6f77ee56376ead1cbfd726813ff4c365
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-29 01:57:52 -0600

    Final

--- bienesraices/anuncios.php
+++ bienesraices/anuncios.php
@@ -19,11 +19,10 @@
         <div class="contenedor-anuncios">
             <?php foreach($propiedades as $propiedad):?>
             <div class="anuncio">
-
                 <picture>
                     <source srcset=<?php echo $propiedad['imagen']?> type="image/webp">
                     <source srcset="<?php echo $propiedad['imagen']?>"  type="image/jpeg">
-                    <img loading="lazy" src="retornoImagen.php?s_id=<?php echo $propiedad["imagen"]?>" type="image/jpeg" alt="anuncio">                    
+                    <img loading="lazy" src="retornoImagen.php?s_id=<?php echo $propiedad["imagen"]?>"  alt="anuncio">                    
                 </picture>
                 <div class="contenido-anuncio">
                     <h3><?php echo $propiedad['titulo']?></h3>
