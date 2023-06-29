9ac6c9ebb2b2c6da453a4ce0d28d13a6f9b2eaa7
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-29 00:02:37 -0600

    Final

--- bienesraices/admin/propiedades/crear.php
+++ bienesraices/admin/propiedades/crear.php
@@ -19,7 +19,7 @@
         $parametros = array(
             "titulo" => $_POST['titulo'],
             "precio" => $_POST['precio'],
-            "imagen" => $rutaArchivoAGuardar,
+            "imagen" => $secureId.$FILES['imagen']['type'],
             "descripcion" => $_POST['descripcion'],
             "habitaciones" => $_POST['habitaciones'],
             "wc" => $_POST['wc'],
