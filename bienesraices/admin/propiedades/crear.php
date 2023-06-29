d141224086005e801a97fd18d249a2f2ef70353e
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-28 19:24:35 -0600

    Final

--- bienesraices/admin/propiedades/crear.php
+++ bienesraices/admin/propiedades/crear.php
@@ -1,13 +1,15 @@
 <?php
     //Base de datos
     include ('./../../aux2.php');
+    echo $imagen;
     $dao2=new DAO();
     $consulta2="SELECT * FROM vendedores";
     $user_access=$dao2->ejecutarConsulta($consulta2);
     if(isset($_POST['titulo'],$_POST['precio'],$_POST['imagen'],$_POST['descripcion'],$_POST['habitaciones'],$_POST['wc'],$_POST['estacionamiento'],$_POST['vendedor'])){
         $dao = new DAO();
         $fecha=date('Y-m-d H:i:s');
-        $consulta="INSERT INTO Propiedades (titulo,precio,imagen,descripcion,habitaciones,wc,estacionamiento,creado,vendedores_id)"."VALUES(:titulo,:precio,:imagen,:descripcion,:habitaciones,:wc,:estacionamiento,:fecha,:id_vendedores)";
+        $consulta="INSERT INTO Propiedades (titulo,precio,imagen,descripcion,habitaciones,wc,estacionamiento,creado,vendedores_id)".
+        "VALUES(:titulo,:precio,:imagen,:descripcion,:habitaciones,:wc,:estacionamiento,:fecha,:id_vendedores)";
 
         $parametros=array("titulo"=>"$_POST[titulo]",
                         "precio"=>"$_POST[precio]",
