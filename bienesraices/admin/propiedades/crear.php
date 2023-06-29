ba32193e06410ea32e9cf5f34f49bf3ec4acbbf2
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-06-28 22:09:39 -0600

    Final

--- bienesraices/admin/propiedades/crear.php
+++ bienesraices/admin/propiedades/crear.php
@@ -4,7 +4,7 @@
     $dao2 = new DAO();
     $consulta2 = "SELECT * FROM vendedores";
     $user_access = $dao2->ejecutarConsulta($consulta2);
-    if (isset($_POST['titulo'], $_POST['precio'], $_POST['imagen'], $_POST['descripcion'], $_POST['habitaciones'], $_POST['wc'], $_POST['estacionamiento'], $_POST['vendedor'])) {
+    if (isset($_POST['titulo'], $_POST['precio'], $_FILES['imagen'], $_POST['descripcion'], $_POST['habitaciones'], $_POST['wc'], $_POST['estacionamiento'], $_POST['vendedor'])) {
         echo "oasd";
         //Crear carpeta
         $nombreImagen = md5(uniqid(rand(),true));
