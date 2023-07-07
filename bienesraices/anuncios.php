dcb0bf83d1102cb061982896bc8bde558906d919
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-07-07 11:38:01 -0600

    Final

--- bienesraices/anuncios.php
+++ bienesraices/anuncios.php
@@ -23,7 +23,7 @@
                 </picture>
                 <div class="contenido-anuncio">
                     <h3><?php echo $propiedad['titulo']?></h3>
-                    <p><?php echo $propiedad['descripcion']?></p>
+                    
                     <p class="precio"><?php echo $propiedad['precio']?></p>
                     <ul class="iconos-caracteristicas">
                         <li>
