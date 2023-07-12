1c50e92c1abcc99da293246731deb761a1c3d2ca
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-07-12 08:39:19 -0600

    version Final

--- bienesraices/admin/index.php
+++ bienesraices/admin/index.php
@@ -50,39 +50,7 @@
 
         
 
-        <!--<a href=" /bienesraices/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>-->
-
-        <table class="propiedades">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Título</th>
-                    <th>Imagen</th>
-                    <th>Precio</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-
-        <tbody> <!-- Mostrar  los Resultados -->
-            <?php foreach($resultadoConsulta as $propiedad): ?>
-            <tr>
-                <td><?php echo $propiedad['id']; ?></td>
-                <td><?php echo $propiedad['titulo']; ?></td>
-                <td><img loading="lazy" src="../retornoImagen.php?s_id=<?php echo $propiedad["imagen"]?>"  alt="<?php echo $propiedad["nombre_archivo"] ?>" title="<?php echo $propiedad["nombre_archivo"] ?>"></td>                
-                <td><?php echo $propiedad['precio']; ?></td>
-                <td>
-                    <form method="POST" class="w-100">
-                        <input type="hidden" name="id" value="<?php echo $propiedad['id']; ?>">
-
-                        <input type="submit" class="boton-rojo-block" value="Eliminar">
-                    </form>
-                    <a href="/../bienesraices/admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
-                </td>
-            </tr>
-            <?php endforeach ?>
-        </tbody>
-        </table>
-
+        
 <?php
         //Cerrar la conexión
     //mysqli_close($);
