053f7f6a1be22d4d4d3c25cbdda7af8cb78c7cd8
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-07-12 08:07:28 -0600

    version Final

--- bienesraices/admin/index.php
+++ bienesraices/admin/index.php
@@ -1,14 +1,97 @@
 <?php
     require '../includes/funciones.php';
+    require '../aux2.php';
+        // //Importa la conexión
+    require '../includes/config/database.php';
+    $db = new DAO();
+    
+        // //Escribir el Query
+    $consulta = "SELECT * FROM propiedades";
+    
+        // //Consultar la BD
+    //$resultadoConsulta = mysqli_query($db, $query);
+    $resultadoConsulta =  $dao1->ejecutarConsulta($consulta);
+        // //Muestra mensaje condicional
+    $resultado = $_GET['resultado'] ?? null;
+    
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $id = $_POST['id'];
+        $id = filter_var($id, FILTER_VALIDATE_INT);
+    
+        if($id) {
+    
+            //Eliminar el archivo
+            $query = "SELECT imagen FROM propiedades WHERE id = :id";
+            $parametros=array("id"=>$id);
+            $resultado = $dao->ejecutarConsulta($consulta,$parametros);
+            $propiedad = mysqli_fetch_assoc($resultado);
+    
+            unlink("./retornoImagen.php?s_id=" . $propiedad['imagen']);
+    
+            //Eliminar la propiedad
+            $consulta = "DELETE FROM propiedades WHERE id = :id";
+            $parametros=array("id"=>$id);
+            $resultado = $dao->ejecutarConsulta($consulta,$parametros);
+    
+            if($resultado) {
+                header('Location: /bienesraices/admin?resultado=3');
+            }
+        }
+    }
+    
+        //Incluye un template
+    require '../includes/funciones.php';
+    incluirTemplates('header');
     incluirTemplates('header');
 ?>
 
     <main class="contenedor seccion">
         <h1>Administrador de Bienes Raices</h1>
-        <a href="../../bienesraices/admin/propiedades/crear.php"  class="boton boton-verde">Nueva propiedad</a>
-        <a href="../../bienesraices/admin/vendedores/crear.php" class="boton boton-verde">Nuevo Vendedor</a>
-    </main>
+
+        <?php if( intval( $resultado ) === 1): ?>  
+            <p class="alerta exito">Anuncio Creado correctamente</p>  
+        <?php elseif ( intval( $resultado ) === 2): ?>
+            <p class="alerta exito">Anuncio Actualizado correctamente</p>
+        <?php elseif ( intval( $resultado ) === 3): ?>
+            <p class="alerta exito">Anuncio Eliminado correctamente</p>
+        <?php endif; ?>
+
+        <a href="<?php $_SERVER['SERVER_NAME'] ?>/bienesraices/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Título</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+        <tbody> <!-- Mostrar  los Resultados -->
+            <?php while( $propiedad = mysqli_fetch_assoc($resultadoConsulta) ): ?>
+            <tr>
+                <td><?php echo $propiedad['id']; ?></td>
+                <td><?php echo $propiedad['titulo']; ?></td>
+                <td><img src="<?php $_SERVER['SERVER_NAME'] ?>/bienesraices/imagenes/<?php echo $propiedad['imagen']; ?>"/></td>
+                <td><?php echo $propiedad['precio']; ?></td>
+                <td>
+                    <form method="POST" class="w-100">
+                        <input type="hidden" name="id" value="<?php echo $propiedad['id']; ?>">
+
+                        <input type="submit" class="boton-rojo-block" value="Eliminar">
+                    </form>
+                    <a href="<?php $_SERVER['SERVER_NAME'] ?>/bienesraices/admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
+                </td>
+            </tr>
+            <?php endwhile; ?>
+        </tbody>
+        </table>
 
 <?php
+        //Cerrar la conexión
+    //mysqli_close($);
+
     incluirTemplates('footer');
 ?>
