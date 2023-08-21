39b8889b8f9f3a25f8282b42045468ba839e64be
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-21 05:11:08 -0600

    version Final

---
+++ bienesraices/usuario.php
@@ -0,0 +1,26 @@
+<?php 
+// Importar la conexion
+//Incluye el header
+//require 'includes/app.php';
+include 'aux2.php';
+$db = new DAO();
+
+
+// Crear un email y password
+$email = "admin@gmail.com";
+$password = "admin1234";
+
+$passwordHash = password_hash($password, PASSWORD_BCRYPT);
+
+// Query para crear el usuario
+$consulta = "INSERT INTO usuarios (email, password) "."VALUES (':email', ':password)";
+
+$parametros=array("email"=>$email,"password"=>$password);
+
+$resultado=$dao->insertatConsulta($consulta,$parametros);
+
+//echo $query;
+
+// Agregarlo a la base de datos
+//mysqli_query($db, $query);
+
