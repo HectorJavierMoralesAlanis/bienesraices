c3ebe39e9b64d587c3ce2369d07b1400a07a8a21
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-21 06:13:26 -0600

    version Final

--- bienesraices/login.php
+++ bienesraices/login.php
@@ -7,7 +7,6 @@
     // Autenticar el usuario
 
     $errores = [];
-
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
         $email =  (filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL));
         $password = (filter_var($_POST['password']));
