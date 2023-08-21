258420bb771b381f81af9f29ebc040a24da8919c
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-08-21 06:20:48 -0600

    version Final

--- bienesraices/login.php
+++ bienesraices/login.php
@@ -19,7 +19,6 @@
         if(!$password){
             $errores[] = "El password es obligatorio";
         }
-
         // Revisar si el usuario existe
         $query = "SELECT * FROM usuarios"."WHERE email = :email";
         $parametros = array("email"=>$email);
